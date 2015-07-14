83f1329d9e6eb522feee02573bfefd2645f93028
Justin Norris <justin.j.norris@gmail.com>
2015-07-14 16:51:31 -0400

    Implement use case: logout

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -13,13 +13,35 @@
             </a>
         </div>
 
-        {{-- Collect the nav links, forms, and other content for toggling --}}
-        <div class="collapse navbar-collapse" id="navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li class="{{ set_active("/login") }}">
-                    <a href="{{ route('auth.getLogin') }}"><i class="fa fa-user fa-fw"></i> Register / Login</a>
-                </li>
-            </ul>
-        </div>
+        {{-- Authenticated links --}}
+        @if(Auth::check())
+
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <i class="fa fa-user fa-fw"></i> Profile <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{ route('auth.getLogout') }}">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+        {{-- Anonymous links --}}
+        @else
+
+            <div class="collapse navbar-collapse" id="navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="{{ set_active("login") }}">
+                        <a href="{{ route('auth.getLogin') }}"><i class="fa fa-user fa-fw"></i> Register / Login</a>
+                    </li>
+                </ul>
+            </div>
+
+        @endif
+
+
     </div>
 </nav>
